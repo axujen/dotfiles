36a1fa2dac0e005bb769d84205924de41305deee
Axujen <axujen@autistici.org>
2016-12-11 12:51:25 +0000

    Fix xinit scripts

--- home/xstartup/xstartup.sh
+++ home/xstartup/xstartup.sh
@@ -2,6 +2,9 @@
 # Startup apps for xorg
 # Should run AFTER the DE/WM is started.
 
+# Settings
+command -v xmodmap          && xmodmap ~/.xmodmaprc &
+
 # Startup applications
 command -v nitrogen			&& nitrogen  --restore &
 command -v blueman-applet	&& blueman-applet &
